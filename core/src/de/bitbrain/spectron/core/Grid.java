5fde8a31989f8a114044bce45b13b4bc45819057
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-07-28 19:18:35 +0200

    Finished grid animations

--- core/src/de/bitbrain/spectron/core/Grid.java
+++ core/src/de/bitbrain/spectron/core/Grid.java
@@ -5,25 +5,39 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenAccessor;
+import aurelienribon.tweenengine.TweenEquations;
+import aurelienribon.tweenengine.TweenManager;
 import de.bitbrain.braingdx.assets.SharedAssetManager;
 import de.bitbrain.spectron.Assets;
 import de.bitbrain.spectron.Colors;
 
 public class Grid {
 
-    public static final float PADDING = 10f;
+    public static final float PADDING = 12f;
 
-    public static final float SCALE = 64f;
+    public static final float SCALE = 80f;
 
     public static class Cell extends Sprite {
 
         private int state;
 
-        private int depth = 4;
+        private int depth = 6;
+
+        private float offset = 0;
 
         public Cell() {
             super(SharedAssetManager.get(Assets.Textures.BLOCK, Texture.class));
             setColor(Colors.EMERALD);
+        }
+
+        public float getOffset() {
+            return offset;
+        }
+
+        public void setOffset(float offset) {
+            this.offset = offset;
         }
 
         public int getDepth() {
@@ -43,14 +57,50 @@ public class Grid {
         }
     }
 
+    private static class CellTween implements TweenAccessor<Cell> {
+
+        public static final int OFFSET = 1;
+        public static final int ALPHA = 2;
+
+
+        @Override
+        public int getValues(Cell target, int tweenType, float[] returnValues) {
+            switch (tweenType) {
+                case OFFSET:
+                    returnValues[0] = target.getOffset();
+                    return 1;
+                case ALPHA:
+                    returnValues[0] = target.getColor().a;
+                    return 1;
+            }
+            return 0;
+        }
+
+        @Override
+        public void setValues(Cell target, int tweenType, float[] newValues) {
+            switch (tweenType) {
+                case OFFSET:
+                    target.setOffset(newValues[0]);
+                    break;
+                case ALPHA:
+                    target.setAlpha(newValues[0]);
+                    break;
+            }
+        }
+    }
+
+    static {
+        Tween.registerAccessor(Cell.class, new CellTween());
+    }
+
     private Cell[][] cells;
 
     private float x, y;
 
     private int xCells, yCells;
 
-    public Grid(float x, float y, int xCells, int yCells) {
-        cells = prepare(xCells, yCells);
+    public Grid(float x, float y, int xCells, int yCells, TweenManager tweenManager) {
+        cells = prepare(xCells, yCells, tweenManager);
         this.x = x;
         this.y = y;
         this.xCells = xCells;
@@ -58,20 +108,21 @@ public class Grid {
     }
 
     public void render(Batch batch, float delta) {
-        for (int x = 0; x < cells.length; ++x) {
-            for (int y = 0; y < cells[x].length; ++y) {
+        for (int x = cells.length - 1; x >=0; --x) {
+            for (int y = cells[x].length - 1; y >= 0; --y) {
                 Cell cell = cells[x][y];
                 cell.setSize(SCALE, SCALE);
                 float cellX = x * cell.getWidth() + getX();
-                float cellY = y * cell.getHeight() + getY();
+                float cellY = y * cell.getHeight() + getY() + cell.getOffset();
                 float width = cell.getWidth();
                 float height = cell.getHeight();
                 Color color = cell.getColor();
-                cell.setColor(Colors.lighten(color, 0.6f));
-                cell.setBounds(cellX + PADDING * x, cellY + PADDING * y - cell.getDepth(), width, height);
+                cell.setColor(Colors.lighten(color, 0.5f));
+                final float PADDING_Y = PADDING * 1.2f;
+                cell.setBounds(cellX + PADDING * x, cellY + PADDING_Y * y - cell.getDepth(), width, height);
                 cell.draw(batch);
                 cell.setColor(color);
-                cell.setBounds(cellX + PADDING * x, cellY + PADDING * y, width, height);
+                cell.setBounds(cellX + PADDING * x, cellY + PADDING_Y * y, width, height);
                 cell.draw(batch);
             }
         }
@@ -98,11 +149,18 @@ public class Grid {
         return y;
     }
 
-    private Cell[][] prepare(int width, int height) {
+    private Cell[][] prepare(int width, int height, TweenManager tweenManager) {
         Cell[][] grid = new Cell[width][height];
+        int iteration = 0;
         for (int x = 0; x < grid.length; ++x) {
             for (int y = 0; y < grid[x].length; ++y) {
-                grid[x][y] = new Cell();
+                Cell cell = new Cell();
+                grid[x][y] = cell;
+                cell.setOffset(-250f);
+                cell.setAlpha(0f);
+                Tween.to(cell, CellTween.OFFSET, 1.5f).target(0f).delay(iteration * 0.02f).ease(TweenEquations.easeOutElastic).start(tweenManager);
+                Tween.to(cell, CellTween.ALPHA, 2.5f).target(1f).delay(iteration * 0.02f).ease(TweenEquations.easeOutExpo).start(tweenManager);
+                iteration++;
             }
         }
         return grid;
--- core/src/de/bitbrain/spectron/screens/IngameScreen.java
+++ core/src/de/bitbrain/spectron/screens/IngameScreen.java
@@ -3,13 +3,19 @@ package de.bitbrain.spectron.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
+import aurelienribon.tweenengine.Tween;
 import de.bitbrain.braingdx.AbstractScreen;
 import de.bitbrain.braingdx.BrainGdxGame;
 import de.bitbrain.braingdx.assets.SharedAssetManager;
+import de.bitbrain.braingdx.tweens.SpriteTween;
 import de.bitbrain.spectron.Assets;
 import de.bitbrain.spectron.Colors;
+import de.bitbrain.spectron.Config;
 import de.bitbrain.spectron.core.Grid;
 
 public class IngameScreen extends AbstractScreen {
@@ -23,7 +29,7 @@ public class IngameScreen extends AbstractScreen {
 
     @Override
     protected void onCreateStage(Stage stage, int width, int height) {
-        grid = new Grid(0, 0, 11, 5);
+        grid = new Grid(0, 0, 10, 4, tweenManager);
         grid.setPosition(width / 2f - grid.getWidth() / 2f, 40f);
     }
 
@@ -36,5 +42,10 @@ public class IngameScreen extends AbstractScreen {
     protected void afterWorldRender(Batch batch, float delta) {
         Texture overlay = SharedAssetManager.get(Assets.Textures.OVERLAY, Texture.class);
         batch.draw(overlay, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    @Override
+    protected Viewport getViewport(int width, int height) {
+        return new FitViewport(Config.APP_WIDTH, Config.APP_HEIGHT);
     }
 }
