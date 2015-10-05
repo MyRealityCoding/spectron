b487b7bb0a52577f17f4c606a47b1f4e463e0b48
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-10-05 11:43:09 +0200

    Correct player movement controls

--- core/src/de/bitbrain/spectron/input/KeyboardInput.java
+++ core/src/de/bitbrain/spectron/input/KeyboardInput.java
@@ -15,22 +15,26 @@ public class KeyboardInput extends InputAdapter {
 
     @Override
     public boolean keyDown(int keycode) {
+        int player = keycode == Input.Keys.W ||
+                keycode == Input.Keys.A ||
+                keycode == Input.Keys.S ||
+                keycode == Input.Keys.D ? 0 : 1;
         switch (keycode) {
             case Input.Keys.W:
             case Input.Keys.UP:
-                controller.move(0, GameObjectController.Move.UP);
+                controller.move(player, GameObjectController.Move.UP);
                 return true;
             case Input.Keys.A:
             case Input.Keys.LEFT:
-                controller.move(0, GameObjectController.Move.LEFT);
+                controller.move(player, GameObjectController.Move.LEFT);
                 return true;
             case Input.Keys.S:
             case Input.Keys.DOWN:
-                controller.move(0, GameObjectController.Move.DOWN);
+                controller.move(player, GameObjectController.Move.DOWN);
                 return true;
             case Input.Keys.D:
             case Input.Keys.RIGHT:
-                controller.move(0, GameObjectController.Move.RIGHT);
+                controller.move(player, GameObjectController.Move.RIGHT);
                 return true;
         }
         return false;
